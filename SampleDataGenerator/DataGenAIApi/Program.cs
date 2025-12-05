00f6794440fa20e409a8383480fcaa5471bd0938
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-12-05 15:04:07 +0530

    Added scalar package for api testing

--- SampleDataGenerator/DataGenAIApi/Program.cs
+++ SampleDataGenerator/DataGenAIApi/Program.cs
@@ -1,19 +1,22 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi();      
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(); 
 }
 
 app.UseHttpsRedirection();
 
 
-
+app.MapGet("/", () => "Hello world");
 
 app.Run();
 

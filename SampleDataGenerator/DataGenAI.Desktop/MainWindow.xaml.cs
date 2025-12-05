fd73c970889a1d711560167433cf4c46dcb07f02
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-12-05 14:28:24 +0530

    wired the funcyionality to the ui

--- SampleDataGenerator/DataGenAI.Desktop/MainWindow.xaml.cs
+++ SampleDataGenerator/DataGenAI.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using SampleDataGeneratorLibrary;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +21,38 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void SubmitButton_Click(object sender, RoutedEventArgs e)
+    {
+        SubmitButton.IsEnabled = false;
+        SubmitButton.Content = "Processing...";
+
+
+        try
+        {
+           using JsonDocument sampleDocument = JsonDocument.Parse(SampleStructure.Text.ToString());
+
+            AIGenerator generator = new(@"C:\Users\shahe\Downloads\Llama-3.2-3B-Instruct-Q4_K_M.gguf");
+            var output = await generator.GetSampleDataAsync(int.Parse(NumberOfRecords.Text), sampleDocument);
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true
+            };
+            string finalJson = JsonSerializer.Serialize(output, options);
+
+            Results.Text = finalJson;
+
+        }
+        catch (Exception ex )
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            
+        }
+        finally
+        {
+            SubmitButton.IsEnabled = true;
+            SubmitButton.Content = "Submit";
+        }
     }
 }
